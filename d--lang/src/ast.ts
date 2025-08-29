e4b132c744c00b4892e458a437a3ccd61784106b
xia <xianyin2014@outlook.com>
2025-08-29 17:56:55 +0800

    added code in the ast

--- d--lang/src/ast.ts
+++ d--lang/src/ast.ts
@@ -6,3 +6,25 @@ export type nType=
     |"call"
     |"unary"
     |"func"
+export interface statment{
+    kind:nType
+}
+export interface code extends statment{
+    kind:"code"
+    body:statment[]
+}
+export interface expr extends statment{}
+export interface bin extends expr{
+    left:expr
+    right:expr
+    operator:string
+    kind:"bin"
+}
+export interface identify extends expr{
+    kind:"identify"
+    symbol:string
+}
+export interface numLit extends expr{
+    kind:"numLit"
+    val:number
+}
